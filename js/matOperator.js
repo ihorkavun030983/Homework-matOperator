f0ece45dc14fc34537081ef1cd95fab6962fa9f7
fantom030983 <ihorkavun1234@gmail.com>
2023-09-27 14:08:20 +0300

    Homewotk 2

---
+++ js/matOperator.js
@@ -0,0 +1,14 @@
+var x=prompt('2');
+var y=prompt('2');
+
+var z=(Number(x)+Number(y));
+alert("Cума чисел: " + z);
+
+var z=(Number(x)-Number(y));
+alert("Різниця чисел: " + z);
+
+var z=(Number(x)*Number(y));
+alert("Добуток чисел: " + z);
+
+var z=(Number(x)/Number(y));
+alert("Ділення чисел: " + z); 
